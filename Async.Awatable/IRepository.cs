f3aa870e7608f0722befffd72230218b03232cfe
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-14 10:18:02 +0800

    excamples with Task.FromResult

---
+++ Async.Awatable/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async.Awatable
+{
+    interface IRepository
+    {
+        Task CleanData();
+    }
+
+    class RepositorySync : IRepository
+    {
+        public Task CleanData()
+        {
+            // operaciones sincronas
+            return Task.CompletedTask;
+        }
+    }
+
+    class RepositoryAsync : IRepository
+    {
+        public async Task CleanData()
+        {
+            // operaciones asincronas
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Async.Awatable/IServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async.Awatable
+{
+    interface IServices
+    {
+        Task<int> GetIntId();
+    }
+
+    class ServicesSync : IServices
+    {
+        Task<int> IServices.GetIntId()
+        {
+            int Id = new Random().Next(1000);
+            return Task.FromResult(Id);
+        }
+    }
+
+    class ServicesAsync : IServices
+    {
+        public async Task<int> GetIntId()
+        {
+            int Id = new Random().Next(1000);
+            await Task.Delay(1);
+            return Id;
+        }
+    }
+
+    class TestServices
+    {
+        public async void Test()
+        {
+            IServices services = new ServicesSync();
+            int syndId = await services.GetIntId();
+
+            services = new ServicesAsync();
+            int asyndId = await services.GetIntId();
+        }
+    }
+}
