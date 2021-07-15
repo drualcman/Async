ba58206d00e69cc90a93db7f34a969881a3b7e75
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-15 08:56:47 +0800

    working with ValueTask

---
+++ Aync.ValueTask/ValueTaskDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aync.ValueTask
+{
+    class ValueTaskDemo
+    {
+        // Necesita mas manejo de memoria porque es tipo referencia y el garbage collector si no tiene
+        // memoria necesita entrar en ejecucion
+        public async Task<int> GetNewProductId()
+        {
+            // operaciones
+            await Task.Delay(3000);
+
+            // logica
+
+            return 10;
+        }
+
+        // Requiere menos memoria porque es tipo valor y el valor se pasa directo por el stack
+        public async ValueTask<int> GetAnotherNewProductId()
+        {
+            // operaciones
+            await Task.Delay(3000);
+
+            // logica
+
+            return 10;
+        }
+
+        interface IRepository<T>
+        {
+            ValueTask<T> GetNew();
+        }
+
+        class RepositorySync<T> : IRepository<T>
+        {
+            public ValueTask<T> GetNew()
+            {
+                var value = default(T);
+
+                // logica
+
+                return new ValueTask<T>(value);
+            }
+        }
+        class RepositoryAsync<T> : IRepository<T>
+        {
+            public async ValueTask<T> GetNew()
+            {
+                var value = default(T);
+
+                // logica
+
+                return await new ValueTask<T>(value);
+            }
+        }
+    }
+}
