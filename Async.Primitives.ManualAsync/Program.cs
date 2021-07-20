e17f2f531d325ee4eeff36378d1571304082bf35
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-20 12:02:29 +0800

    pmimitives

---
+++ Async.Primitives.ManualAsync/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async.Primitives.ManualResetEventSlimDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoManualResetEventSlim();
+        }
+
+        static void DoManualResetEventSlim()
+        {
+            // No señalizado (NonSignaled)
+            ManualResetEventSlim M1 = new ManualResetEventSlim();
+
+            // No señalizado (NonSignaled) explicito
+            ManualResetEventSlim M2 = new ManualResetEventSlim(false);
+
+            // Señalizado (Signaled)
+            ManualResetEventSlim M3 = new ManualResetEventSlim(true);
+
+            Task T1 = Task.Run(() =>
+            {
+                M1.Wait();
+                Console.WriteLine("M1 esta en estado señalizado.");
+                Console.WriteLine("Estable estado NonSignaled al objeto M3....");
+                M3.Reset();         //reset = set NonSignaled
+                
+                Console.WriteLine("Estable estado Signaled al objeto M2....");
+                M2.Set();           //set = set Signaled
+            });
+
+            Console.WriteLine("En el hilo principal");
+            Console.WriteLine($"Estado de M3 es Signaled? {M3.IsSet}");
+            Console.WriteLine("El hilo principal señalizara a M1");
+            M1.Set();
+            
+            Console.WriteLine("El hilo principal espera a que M2 sea señalizado");
+            M2.Wait();
+            Console.WriteLine("En el hilo principal M2 ha sido señalizado");
+            Console.WriteLine($"Estado de M3 es Signaled? {M3.IsSet}");
+
+            T1.Wait();
+            // es buena practica siempre hacer el dispose the los objectos ManualResetEventSlim
+            M1.Dispose();
+            M2.Dispose();
+            M3.Dispose();
+        }
+    }
+}
---
+++ Async.Primitives.SemaphoreSlimDemo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async.Primitives.SemaphoreSlimDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseSemaphoreSlim();
+        }
+
+        static void UseSemaphoreSlim()
+        {
+            SemaphoreSlim S = new SemaphoreSlim(2);
+
+            bool entered = S.Wait(1000);
+
+            Console.WriteLine($"El hilo no tuvo que esperar 1000 milisegundos: {entered}  {S.CurrentCount}");
+
+            entered = S.Wait(1000);
+            Console.WriteLine($"Segundo acceso exitoso: {entered}  {S.CurrentCount}");
+
+            Console.WriteLine($"Aqui ya no hay accesos y el hilo tendra que esperar...");
+
+            entered = S.Wait(1000);
+            Console.WriteLine($"Tercer acceso exitoso: {entered}  {S.CurrentCount}");
+            
+            S.Release();            // incrementa el contador en 1 cada vez que es invocado
+            entered = S.Wait(1000);
+            Console.WriteLine($"Despues del release el acceso exitoso: {entered} {S.CurrentCount}");
+
+            Task T = Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Tarea liberando el semaforo");
+                S.Release();
+            });
+
+            T.Wait();
+            Console.WriteLine($"Accesos concurrentes disponibles: {S.CurrentCount}");
+            S.Dispose();
+        }
+    }
+
+    class kk : IDisposable
+    {
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~kk()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
